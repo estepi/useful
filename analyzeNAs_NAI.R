ad95eb6a8458bbe8daddce5f2751c14bb7e490a2
estepi <soyestepi@gmail.com>
2018-06-18 16:27:10 +0200

    version 1 published

--- analyzeNAs_NAI.R
+++ analyzeNAs_NAI.R
@@ -1,3 +1,14 @@
 #finalMatrixCOmparison
-getwd()
+library(data.table)
+library(stringr)
+INCFile<-"test.tab"
+OUTFile<-"test_NA_NewN3.tab"
+source("replaceNA.R")
+replaceNA(INCFile, OUTFile)
 
+
+INCFile<-"test.tab"
+OUTFile1<-"test_NA_NewN3.tab"
+OUTFile2<-"test_NA_NewN3_NAI.tab"
+source("replaceNAI.R")
+replaceNAI(INCFile, OUTFile1,OUTFile2)

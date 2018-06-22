dd819f4a26f96bc74fbad66ad467f9e915306a1d
estepi <soyestepi@gmail.com>
2018-06-22 11:38:20 +0200

    other features

--- biomaRt_geneFeatures.R
+++ biomaRt_geneFeatures.R
@@ -1,5 +1,5 @@
-#
 library(biomaRt)
+library(data.table)
 listMarts()
 ensembl = useMart("ENSEMBL_MART_ENSEMBL")
 listDatasets(ensembl)
@@ -35,10 +35,44 @@ Glength<-df1$end_position - df1$start_position
 finalDf<-data.frame(df1, numOfEx[ii], Glength)
 #######################################################
 vtnames<-read.csv("completeConversionTable_ENSEMBL_VAST.txt", sep="\t")
-head(vtnames)
 vv<-match(finalDf$ensembl_gene_id, vtnames$FGeneID)
 length(which(is.na(vv)))#0
 finalDf$vtools<-as.character(vtnames$VT[vv]); head(finalDf)
 write.table(finalDf,"NumOfExons.tab", sep="\t", quote=FALSE, col.names = NA)
+###############################################################
+df3<-getBM(attributes = 
+             c("ensembl_gene_id",
+               "go_id","name_1006","definition_1006"),
+           filters    = "ensembl_gene_id",
+           values     = ensIds$Ensemble.Gene.ID, 
+           mart       = ensembl)
+#######################################################
+df4<-getBM(attributes = 
+             c("ensembl_gene_id",
+               "go_id","name_1006","definition_1006"),
+           filters    = "ensembl_gene_id",
+           values     = as.character(Hsa.Id[,1]), 
+           mart       = ensembl)
 
+#######################################################
+vtnames<-read.csv("completeConversionTable_ENSEMBL_VAST.txt", sep="\t")
+df4<-getBM(attributes = 
+             c("ensembl_gene_id",
+               "go_id","name_1006"),
+           filters    = "ensembl_gene_id",
+           values     = as.character(vtnames$FGeneID), 
+           mart       = ensembl); head(df4)
+ii<-match(df4$ensembl_gene_id, vtnames$FGeneID)
+dfFinal<-cbind(df4, vtnames$VT[ii])
+###########################################################
+HsVT<-read.csv("Hsa.ID.names.txt", sep="\t")
+df5<-getBM(attributes = 
+             c("ensembl_gene_id",
+               "go_id","name_1006"),
+           filters    = "ensembl_gene_id",
+           values     = as.character(HsVT[,1]), 
+           mart       = ensembl);
+ii<-match(df5$ensembl_gene_id, HsVT[,1])
+dfFinal<-cbind(df5, HsVT[ii,2])
 
+144/3
